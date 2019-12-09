2269cca6235015d8a5b74912fd2e5ee0f991fb62
sitikarimatul <siti.17411004@student.ubl.ac.id>
2019-12-09 17:07:35 +0700

    first commit

---
+++ src/main/java/server/ConfigUrl.java
@@ -0,0 +1,8 @@
+package server;
+
+public class ConfigUrl {
+    public static String baseurl = "http://192.168.56.1:5000";
+            public static String getAllMhs = baseurl+ "/mahasiswa/getAllMhs";
+            public static String inputDataMhs=baseurl+ "/mahasiswa/insert";
+
+}
